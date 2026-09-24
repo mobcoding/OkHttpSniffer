fca3b4e3ae4af89eeec71489cedadc0417af5eef
zhoutl <tianling.zhou@mob.cool>
2026-09-24 20:27:40 +0800

    Avoid generated ToolWindowFactory deprecated and experimental API bridges

---
+++ src/main/java/com/itkacher/DebuggerToolWindowFactory.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2018 LocaleBro.com [Ievgenii Tkachenko(gektor650@gmail.com)]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.itkacher;
+
+import com.intellij.ide.util.PropertiesComponent;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.itkacher.views.form.MainForm;
+import org.jetbrains.annotations.NotNull;
+
+// Java inherits platform defaults without Kotlin-generated forwarding methods.
+public final class DebuggerToolWindowFactory implements ToolWindowFactory, DumbAware {
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        PluginPreferences preferences = new PluginPreferences(PropertiesComponent.getInstance(project));
+        MainForm mainForm = new MainForm();
+        AdbController adbController = new AdbController(mainForm, project, preferences);
+        Disposer.register(project, adbController);
+        toolWindow.getComponent().add(mainForm.getPanel());
+    }
+}
